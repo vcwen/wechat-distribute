bc4f465155fbb21631574ca5f6e356839b9287b7
vincent.wen <wenwei1202@gmail.com>
2017-04-07 15:39:00 +0800

    export entry

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import ClientRouter from './lib/ClientRouter'
+import DataSource from './lib/DataSource'
+import Disapachter from './lib/Dispatcher'
+import MessageRouter from './lib/MessageRouter'
+import SimpleDataSource from './lib/SimpleDataSource'
+import Client from './model/Client'
+import Message from './model/Message'
+import Route from './model/Route'
+import WechatAccount from './model/WechatAccount'
+
+export {ClientRouter, DataSource, Disapachter, SimpleDataSource,
+  MessageRouter, Client, Message, Route, WechatAccount}
